f47dcf63036bd68ecf57ef05e05de34531e363af
Amanda Ravelo Cabrera <aravelo@omniainfosys.com>
2023-07-24 13:40:03 +0100

    feat: first problem has done and the tests have pased correctly

--- calculator.py
+++ calculator.py
@@ -1,9 +1,17 @@
 def calculator(number='0'):
+    if ',' in number:
+        array_number = number.split(',')
+        result = 0
+        for number in array_number:
+            result += int(number)
+        return str(result)
+
     return number
 
 
 if __name__ == '__main__':
     print(calculator())
     print(calculator('1'))
+    print(calculator('1,2,3'))
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,9 @@ class Test(TestCase):
     def test_calculator_should_return_the_same_number_when_receive_only_one_number(self):
         self.assertEqual(calculator('1'), '1')
 
+    def test_calculator_add_all_the_numbers_separated_by_commas(self):
+        self.assertEqual(calculator('1,2,3'), '6')
+
 
 """
 class Test(TestCase):
