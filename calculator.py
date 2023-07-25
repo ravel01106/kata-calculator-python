4c6c0a17a9e341e1aad691101e35e4948df88cd8
Amanda Ravelo Cabrera <aravelo@omniainfosys.com>
2023-07-25 17:42:24 +0100

    feat: the fourth problem has done and the tests have passed correctly

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-def contain_two_different_delimiter(numbers):
+def position_two_delimiters_together(numbers):
     for x in range(1, len(numbers)):
         before_character = numbers[x - 1]
         character = numbers[x]
@@ -8,21 +8,33 @@ def contain_two_different_delimiter(numbers):
 
 
 def add(numbers='0'):
+    separator = ','
     if not numbers[len(numbers) - 1].isnumeric():
         return "Number expected but EOF found."
 
-    if ',' in numbers:
-        position = contain_two_different_delimiter(numbers)
-        if position != -1:
-            if "\n" in numbers:
-                position = numbers.index("\n")
-                return 'Number expected but "\n" found at position {}.'.format(position)
-            else:
-                return 'Number expected but "," found at position {}.'.format(position)
+    if numbers.startswith("//"):
+        newline_position = numbers.index("\n")
+        separator = numbers[2:newline_position]
+        numbers = numbers[(newline_position + 1):len(numbers)]
 
-        numbers = numbers.replace("\n", ",")
-        print(numbers)
-        array_numbers = numbers.split(',')
+    if len(numbers) > 1:
+        if separator == ',':
+            position = position_two_delimiters_together(numbers)
+            if position != -1:
+                if "\n" in numbers:
+                    position = numbers.index("\n")
+                    return 'Number expected but "\n" found at position {}.'.format(position)
+                else:
+                    return 'Number expected but "," found at position {}.'.format(position)
+
+            numbers = numbers.replace("\n", ",")
+
+        for character in numbers:
+            if character not in separator and not character.isnumeric():
+                return "'{}' expected but '{}' found at position {}.".format(
+                    separator, character, numbers.index(character))
+
+        array_numbers = numbers.split(separator)
         result = 0
         for numbers in array_numbers:
             result += int(numbers)
@@ -33,8 +45,3 @@ def add(numbers='0'):
 
 if __name__ == '__main__':
     print(add())
-    print(add('1'))
-    print(add('1,2,3'))
-    print(add('1\n2,3'))
-
-
--- test_calculator.py
+++ test_calculator.py
@@ -15,11 +15,19 @@ class Test(TestCase):
     def test_calculator_add_all_the_numbers_separated_by_commas_and_newlines(self):
         self.assertEqual(add('1\n2,3'), '6')
 
-    def test_calculator_send_a_error_message_when_there_are_two_delimiters_together(self):
+    def test_calculator_send_a_error_when_there_are_two_delimiters_together(self):
         self.assertEqual(add('175.2,\n35'), 'Number expected but "\n" found at position 6.')
 
-    def test_calculator_send_a_error_message_when_there_are_a_delimiter_at_the_end(self):
+    def test_calculator_send_a_error_when_there_are_a_delimiter_at_the_end(self):
         self.assertEqual(add('1,3,'), 'Number expected but EOF found.')
+
+    def test_calculator_add_the_numbers_with_custom_delimiters(self):
+        self.assertEqual(add('//;\n1;2'), '3')
+        self.assertEqual(add('//|\n1|2|3'), '6')
+        self.assertEqual(add('//sep\n2sep3'), '5')
+
+    def test_calculator_send_a_error_when_there_are_two_delimiter(self):
+        self.assertEqual(add('//|\n1|2,3'), '\'|\' expected but \',\' found at position 3.')
 
 
 """
