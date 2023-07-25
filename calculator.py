535e78a2762bea3f9dc38654ac5c5060b4a4eb3c
Amanda Ravelo Cabrera <aravelo@omniainfosys.com>
2023-07-25 16:42:35 +0100

    refactor: a new function was created to check if the string contains two delimiters

--- calculator.py
+++ calculator.py
@@ -1,16 +1,21 @@
-def calculator(numbers='0'):
+def contain_two_different_delimiter(numbers):
+    for x in range(1, len(numbers)):
+        before_character = numbers[x - 1]
+        character = numbers[x]
+        if not character.isnumeric() and not before_character.isnumeric():
+            return x
+    return -1
 
+
+def add(numbers='0'):
     if ',' in numbers:
-        for x in range(1, len(numbers)):
-            before_character = numbers[x - 1]
-            character = numbers[x]
-            if not character.isnumeric() and not before_character.isnumeric():
-                if "\n" in numbers:
-                    position = numbers.index("\n")
-                    return 'Number expected but "\n" found at position {}.'.format(position)
-                else:
-                    position = x
-                    return 'Number expected but "," found at position {}.'.format(position)
+        position = contain_two_different_delimiter(numbers)
+        if position != -1:
+            if "\n" in numbers:
+                position = numbers.index("\n")
+                return 'Number expected but "\n" found at position {}.'.format(position)
+            else:
+                return 'Number expected but "," found at position {}.'.format(position)
 
         numbers = numbers.replace("\n", ",")
         print(numbers)
@@ -24,9 +29,9 @@ def calculator(numbers='0'):
 
 
 if __name__ == '__main__':
-    print(calculator())
-    print(calculator('1'))
-    print(calculator('1,2,3'))
-    print(calculator('1\n2,3'))
+    print(add())
+    print(add('1'))
+    print(add('1,2,3'))
+    print(add('1\n2,3'))
 
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,22 +1,23 @@
 from unittest import TestCase
-from calculator import calculator
+from calculator import add
 
 
 class Test(TestCase):
     def test_calculator_return_0_when_no_receive_numbers(self):
-        self.assertEqual(calculator(), '0')
+        self.assertEqual(add(), '0')
 
     def test_calculator_should_return_the_same_number_when_receive_only_one_number(self):
-        self.assertEqual(calculator('1'), '1')
+        self.assertEqual(add('1'), '1')
 
     def test_calculator_add_all_the_numbers_separated_by_commas(self):
-        self.assertEqual(calculator('1,2,3'), '6')
+        self.assertEqual(add('1,2,3'), '6')
 
     def test_calculator_add_all_the_numbers_separated_by_commas_and_newlines(self):
-        self.assertEqual(calculator('1\n2,3'), '6')
+        self.assertEqual(add('1\n2,3'), '6')
 
     def test_calculator_send_a_error_message_when_there_are_two_delimiters_together(self):
-        self.assertEqual(calculator('175.2,\n35'), 'Number expected but "\n" found at position 6.')
+        self.assertEqual(add('175.2,\n35'), 'Number expected but "\n" found at position 6.')
+
 
 """
 class Test(TestCase):
