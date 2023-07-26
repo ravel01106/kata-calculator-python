50f66b2d2f2562aa51a4bf21a4e3332778bdc7b5
ravel01106 <ravello2002@gmail.com>
2023-07-27 00:00:07 +0100

    refactor: some new functions were created to simplify and better understand the code and the tests have passed correctly

--- calculator.py
+++ calculator.py
@@ -7,41 +7,67 @@ def position_two_delimiters_together(numbers):
     return -1
 
 
-def add(numbers='0'):
-    separator = ','
-    if not numbers[len(numbers) - 1].isnumeric():
-        return "Number expected but EOF found."
+def custom_delimiter(numbers, separator):
+    newline_position = numbers.index("\n")
+    separator = numbers[2:newline_position]
+    return numbers[(newline_position + 1):len(numbers)], separator
 
-    if numbers.startswith("//"):
-        newline_position = numbers.index("\n")
-        separator = numbers[2:newline_position]
-        numbers = numbers[(newline_position + 1):len(numbers)]
 
-    if len(numbers) > 1:
-        if separator == ',':
-            position = position_two_delimiters_together(numbers)
-            if position != -1:
-                if "\n" in numbers:
-                    position = numbers.index("\n")
-                    return 'Number expected but "\n" found at position {}.'.format(position)
-                else:
-                    return 'Number expected but "," found at position {}.'.format(position)
+def send_error_delimiters_together(numbers, separator, is_custom_delimiter):
+    if separator == ',':
+        position = position_two_delimiters_together(numbers)
+        if position != -1:
+            if "\n" in numbers:
+                position = numbers.index("\n")
+                return 'Number expected but "\n" found at position {}.'.format(position)
 
-            numbers = numbers.replace("\n", ",")
+            return 'Number expected but "," found at position {}.'.format(position)
 
+    elif is_custom_delimiter:
         for character in numbers:
             if character not in separator and not character.isnumeric():
                 return "'{}' expected but '{}' found at position {}.".format(
                     separator, character, numbers.index(character))
+    return ''
 
-        array_numbers = numbers.split(separator)
-        result = 0
-        for numbers in array_numbers:
-            result += int(numbers)
-        return str(result)
+
+def add_numbers(numbers, separator):
+    array_numbers = numbers.split(separator)
+    result = 0
+    for numbers in array_numbers:
+        result += int(numbers)
+    return str(result)
+
+
+def calculator(numbers='0'):
+    separator = ','
+    is_custom_delimiter = False
+    have_delimiter_at_end = not numbers[len(numbers) - 1].isnumeric()
+    have_custom_delimiter =numbers.startswith("//")
+    is_not_only_one_number = len(numbers) > 1
+
+    if have_delimiter_at_end:
+        return "Number expected but EOF found."
+
+    if have_custom_delimiter:
+        is_custom_delimiter = True
+        numbers, separator = custom_delimiter(numbers, separator)
+
+    is_a_comma_separator = separator == ','
+
+    if is_not_only_one_number:
+        have_delimiter_together = send_error_delimiters_together(numbers, separator, is_custom_delimiter) != ''
+
+        if have_delimiter_together:
+            return send_error_delimiters_together(numbers, separator, is_custom_delimiter)
+
+        if is_a_comma_separator:
+            numbers = numbers.replace("\n", ",")
+
+        return add_numbers(numbers, separator)
 
     return numbers
 
 
 if __name__ == '__main__':
-    print(add())
+    print(calculator())
--- test_calculator.py
+++ test_calculator.py
@@ -1,37 +1,30 @@
 from unittest import TestCase
-from calculator import add
+from calculator import calculator
 
 
 class Test(TestCase):
     def test_calculator_return_0_when_no_receive_numbers(self):
-        self.assertEqual(add(), '0')
+        self.assertEqual(calculator(), '0')
 
     def test_calculator_should_return_the_same_number_when_receive_only_one_number(self):
-        self.assertEqual(add('1'), '1')
+        self.assertEqual(calculator('1'), '1')
 
-    def test_calculator_add_all_the_numbers_separated_by_commas(self):
-        self.assertEqual(add('1,2,3'), '6')
+    def test_calculator_add_the_numbers_separated_by_commas(self):
+        self.assertEqual(calculator('1,2,3'), '6')
 
-    def test_calculator_add_all_the_numbers_separated_by_commas_and_newlines(self):
-        self.assertEqual(add('1\n2,3'), '6')
+    def test_calculator_add_the_numbers_separated_by_commas_and_newlines(self):
+        self.assertEqual(calculator('1\n2,3'), '6')
 
-    def test_calculator_send_a_error_when_there_are_two_delimiters_together(self):
-        self.assertEqual(add('175.2,\n35'), 'Number expected but "\n" found at position 6.')
+    def test_calculator_send_an_error_when_there_are_two_delimiters_together(self):
+        self.assertEqual(calculator('175.2,\n35'), 'Number expected but "\n" found at position 6.')
 
-    def test_calculator_send_a_error_when_there_are_a_delimiter_at_the_end(self):
-        self.assertEqual(add('1,3,'), 'Number expected but EOF found.')
+    def test_calculator_send_an_error_when_there_is_a_delimiter_at_the_end(self):
+        self.assertEqual(calculator('1,3,'), 'Number expected but EOF found.')
 
-    def test_calculator_add_the_numbers_with_custom_delimiters(self):
-        self.assertEqual(add('//;\n1;2'), '3')
-        self.assertEqual(add('//|\n1|2|3'), '6')
-        self.assertEqual(add('//sep\n2sep3'), '5')
+    def test_calculator_add_the_numbers_that_are_separated_custom_delimiters(self):
+        self.assertEqual(calculator('//;\n1;2'), '3')
+        self.assertEqual(calculator('//|\n1|2|3'), '6')
+        self.assertEqual(calculator('//sep\n2sep3'), '5')
 
-    def test_calculator_send_a_error_when_there_are_two_delimiter(self):
-        self.assertEqual(add('//|\n1|2,3'), '\'|\' expected but \',\' found at position 3.')
-
-
-"""
-class Test(TestCase):
-    def test_calculator(self):
-        self.fail()
-"""
+    def test_calculator_send_a_error_when_there_are_two_different_delimiter(self):
+        self.assertEqual(calculator('//|\n1|2,3'), '\'|\' expected but \',\' found at position 3.')
